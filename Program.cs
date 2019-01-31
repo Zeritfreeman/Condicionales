5e9ea0ac0be1cde156b3e3b2a43fcf4f9d97f99b
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-01-30 21:19:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicional03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double smmlv = 828116.0;
+            Console.WriteLine("Ingrese su salario");
+            double s = double.Parse(Console.ReadLine());
+
+            if (s < (2.0 * smmlv))
+            {
+                Console.WriteLine("A usted le corresponde la Tarifa A");
+            }
+            else if (s < (4.0 * smmlv)) 
+            {
+                Console.WriteLine("A usted le corresponde la Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("A usted le corresponde la Tarifa C");
+            }
+        }
+    }
+}
